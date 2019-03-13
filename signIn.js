1b11a0f070c9dc3aefe162a6875cb38350bf4a18
EstherSeyi <sheyiogundijo@gmail.com>
2019-03-13 10:22:39 +0100

    fixed sign in

--- signIn.js
+++ signIn.js
@@ -8,17 +8,19 @@
       method: "GET",
       url: `http://localhost:3000/users?stafflastName=${staffLastName}`
     }).done(function(data) {
-      console.log(data);
+      //console.log(data);
       if (staffLastName === data[0].stafflastName && staffId === data[0].staffId && password === data[0].password) {
         location.replace('./index.html');
       } else {
         location.reload(true);
         alert('wrong details');
       }
-    }).fail(function() {
-      location.reload(true);
-      alert('Oops! there is an error.');
-    });
+    })
+    //fail(function() {
+    //  // location.reload(true);
+    //   alert('Oops! there is an error.');
+    // });
+    alert("unauthorized user");
   });
 
 
